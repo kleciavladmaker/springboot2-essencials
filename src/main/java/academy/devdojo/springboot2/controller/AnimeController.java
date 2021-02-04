812c9992e47616d2854d479eebf4174832a4e78d
Klecia Vladmaker <klecia.vladmaker@hotmail.com>
2021-02-04 15:04:24 -0300

    Pacote service e Método GET

--- src/main/java/academy/devdojo/springboot2/controller/AnimeController.java
+++ src/main/java/academy/devdojo/springboot2/controller/AnimeController.java
@@ -1,10 +1,14 @@
 package academy.devdojo.springboot2.controller;
 
 import academy.devdojo.springboot2.domain.Anime;
+import academy.devdojo.springboot2.service.AnimeService;
 import academy.devdojo.springboot2.util.DateUtil;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,24 +16,22 @@ import java.time.LocalDateTime;
 import java.util.List;
 
 @RestController
-@RequestMapping("anime")
+@RequestMapping("animes")
 @Log4j2
 @RequiredArgsConstructor
 public class AnimeController {
 
     private final DateUtil dateUtil;
+    private final AnimeService animeService;
 
-
-    //localhost:8080/anime/List
-    @GetMapping(path = "List")
-    public List<Anime> list () {
+    @GetMapping
+    public ResponseEntity<List<Anime>> list () {
         log.info(dateUtil.formatLocalDateTimeToDatabaseStyle(LocalDateTime.now()));
-        return List.of(new Anime("Boku no Hero"), new Anime("Berserk"));
+        return ResponseEntity.ok(animeService.listAll());
     }
 
-    @GetMapping(path = "List2")
-    public List<Anime> list2 () {
-        log.info(dateUtil.formatLocalDateTimeToDatabaseStyle(LocalDateTime.now()));
-        return List.of(new Anime("Boku no Hero"), new Anime("Berserk"), new Anime("One Piece"));
+    @GetMapping(path = "/{id}")
+    public ResponseEntity<Anime> findById (@PathVariable long id) {
+        return ResponseEntity.ok(animeService.findById(id));
     }
 }
