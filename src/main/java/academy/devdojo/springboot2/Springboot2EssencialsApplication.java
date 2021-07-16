49c24b559c2764455b559c4eb2a192416f271105
Klecia Vladmaker <klecia.vladmaker@hotmail.com>
2021-07-16 20:05:58 -0300

    Initial commit

---
+++ src/main/java/academy/devdojo/springboot2/Springboot2EssencialsApplication.java
@@ -0,0 +1,13 @@
+package academy.devdojo.springboot2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot2EssencialsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Springboot2EssencialsApplication.class, args);
+    }
+
+}
